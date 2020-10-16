718d32f89bc8083af26c380a2b534b4131350b5a
Oseyeon-1 <72958869+Oseyeon-1@users.noreply.github.com>
2020-10-16 13:44:53 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -3,62 +3,49 @@
 
 /************* include library **************/
 var express = require('express');
-var mysql   = require('mysql');
+var mysql =    require('mysql');
 var api     = express();
-
 
 /************* Routing **************/
 //api Index
 api.get('/', (req, res, next) => {
+    res.send("Welcome is API Fucntion");
 
-/*
-var dbInfo = {
 
-    host: 'ls-4ab94082220316df9f6805e2cb989baaea4e9715.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'dbmasteruser',
-    password:'angksehwjsqq',
-    database: 'BU',
-    multipleStatements: true
-}
-*/ 
+/************* Routing **************/
+//api Index
+    var connection = mysql.createConnection({
+        host: 'ls-712a3de0f216372c332622b5ed5c6f22fe2f67bd.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
+        port: '3306',
+        user: 'dbmasteruser',
+        password:' buackr!!##',
+        database: 'BU',
+        multipleStatements: true
+    });
 
-    connection.connect();
-    connection.query('SELECT * FROM sensor_data ', function (error, results, fields) {
+    var connerction = mysql.createConnection({
+        host : dbInfo.host,
+        user : dbInfo.user,
+        password : dbInfo.password,
+        database : dbInfo.database
+    })
+    connection.connet();
+    connection.query('SELECT * FROM sensor-data' , function (error, results, fields){
         if (error) {
             console.log(error);
         }
 
         console.log(results);
     });
-    connection.end();    
-    res.send("Welcome is API Fucntion");
+    connection.end();
+    res.send("Welcome is API Function");
 });
-
-/************* Routing **************/
-//api Index
-
-// get
-api.get('/hello2', (req, res, next) => {
+    api.get('/hello', (req, res, next) => {
     var pId = "0";
     if(req.query.id !== null && req.query.id !== undefined){
         pId = req.query.id;
     }
-
-    res.send("510호 강의실 시설 좋아요"+ pId);
-});
-// post
-api.post('/hello', (req, res, next) => {
-    var pId = "0";
-/*
-    name ,
-    number 
-*/
-    console.log("body.id = "+req.body.id);
-    if(req.body.id !== null && req.body.id !== undefined){
-        pId = req.body.id;
-    }    
-    res.send("백석대학교 :"+ pId + "정보는? ");
+    res.send("HTTP GET > Hello Nodejs");
 });
 
 api.post('/insSensor', (req, res, next) => {
@@ -71,8 +58,6 @@ api.post('/insSensor', (req, res, next) => {
     sql += " ('"+ sensorType +"', "+ sensorValue +", '"+ userId +"', now() , now()) ";
     console.log(sql);
     connection.connect();
-
-    console.log("init start");
     connection.query(sql , function(error, results, fields){
 
         console.log(error);
@@ -80,8 +65,11 @@ api.post('/insSensor', (req, res, next) => {
         res.send(results);
     })
 
-});
+    //req.body.sensorIdx
+    //req.body.sensorType
+    //req.body.sensorValue
 
+})
 //Query String
 // ex) http://localhost/api/echo?param1=123&param2=321
 api.get('/query_echo', (req, res, next) => {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 /************* include library **************/
 var express             = require('express');
 var path                = require('path');
+const { query } = require('express');
 var server              = express();
 
 /************* view engine setup **************/
@@ -30,11 +31,10 @@ server.get('/study', (req, res, next) => {
 
     // http://localhost/study?id=1234&name=백석대
 
-    console.log(req.query.name);
     let student = {
         id : 0,
-        name : "test",
-        age : 5
+        name : "손님",
+        age : 21
     }
 
     if(req.query.id !== null && req.query.id !== undefined){
@@ -47,9 +47,9 @@ server.get('/study', (req, res, next) => {
     if(req.query.age !== null && req.query.age !== undefined){
         student.age = req.query.age;
     }
-    
+  
+  
     console.log(student);
-    console.log("console.log 는 이렇게 사용합니다.");
     res.render("iWillStudy.html", student);
 });
 
@@ -67,36 +67,21 @@ server.get('/99dan',  (req, res, next) => {
     if(req.query.primary !== null && req.query.primary !== undefined){
         gugudan.primary = req.query.primary;
     }
-
-    /* integer check */
-    let val = parseInt(req.query.primary);
-    /*
-    if(조건){
-        참일때 실행되는 섹타
-    }else{
-        거짓일때 싫애되는 섹터
-    }
-
-    */
-    //console.log(Number.isInteger(val));
+    /* interger check */
+    let val = parseInt(req,query.primary);
+   // console.log(Number.isInteger(parseInt(req.query.primary)));
     if( Number.isInteger(val) == true  ){
 
-        // 2단부터 9단까지만 구동되게 
-        //if(val >  && val  )
-        if(val > 1 && val < 10){
-            res.render("99dan.html", gugudan); 
+        // 2단부터 9단까지만 구동되게
+        if(val >1 && val < 10){
+            res.render("99dan.html", gugudan);
         }else{
-            res.send("유효하지 않은 값입니다. (2~9)");
-
+            res.send("유효하지 않은 값입니다.")
         }
-        
-
-    }else{
-
-        res.send("숫자가 아닙니다.");
-    }
+    }else
+        res.send("숫자가 아닙니다.")
   
-    
+
 });
 
 module.exports = server;
