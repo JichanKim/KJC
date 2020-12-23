80604376e67ecf52f2c24028e24b7c0bafb6129f
Jichan <rlawlcks1025@gmail.com>
2020-12-23 12:53:36 +0900

    12월23일 수정

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ api.get('/', (req, res, next) => {
         host: 'ls-712a3de0f216372c332622b5ed5c6f22fe2f67bd.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
         port: '3306',
         user: 'dbmasteruser',
-        password:'buackr!!##',
+        password:'angksehwjsqq',
         database: 'BU',
         multipleStatements: true
     };
